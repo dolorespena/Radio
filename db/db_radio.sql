e2c8bec40c0857e1a4e53787fb550ff25f721a9c
Mauricio Oruezabal <moruezabal@gmail.com>
2020-05-06 10:46:59 -0300

    DB columnistas actualizada y redireccionada

---
+++ db/db_radio.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-05-2020 a las 15:44:01
+-- Versión del servidor: 10.1.33-MariaDB
+-- Versión de PHP: 7.2.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_radio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `columnista`
+--
+
+CREATE TABLE `columnista` (
+  `id_columnista` int(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `profesion` varchar(100) NOT NULL,
+  `descripcion` varchar(300) NOT NULL,
+  `url_imagen` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `columnista`
+--
+
+INSERT INTO `columnista` (`id_columnista`, `nombre`, `profesion`, `descripcion`, `url_imagen`) VALUES
+(6, 'Felipe Pigna', 'Historiador', 'Historias de nuestra historia', '/img/profile/pigna.jpg'),
+(7, 'Alejandro Dolina', 'Humorista', 'La venganza será terrible', '/img/profile/dolina.jpg'),
+(8, 'Gabriel Rolón', 'Psicólogo', 'Palabra Plena', 'img/profile/rolon.jpg'),
+(9, 'Paulina Cocina', 'Cocinera', 'Receta en 30 minutos', 'img/profile/paulina.jpg'),
+(10, 'Darío Sztajnszrajber', 'Filósofo', 'Pienso luego existo', 'img/profile/sztajnszrajber.jpg'),
+(11, 'Eduardo Sacheri', 'Escritor', 'Libros recomendados', 'img/profile/sacheri.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `podcast`
+--
+
+CREATE TABLE `podcast` (
+  `id_podcast` int(10) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `descripcion` varchar(500) NOT NULL,
+  `url_audio` varchar(100) NOT NULL,
+  `fecha` date NOT NULL,
+  `duracion` int(10) NOT NULL,
+  `tag` varchar(20) DEFAULT NULL,
+  `invitado` varchar(20) DEFAULT NULL,
+  `id_columnista_fk` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `columnista`
+--
+ALTER TABLE `columnista`
+  ADD PRIMARY KEY (`id_columnista`);
+
+--
+-- Indices de la tabla `podcast`
+--
+ALTER TABLE `podcast`
+  ADD PRIMARY KEY (`id_podcast`),
+  ADD KEY `id_columnista_fk` (`id_columnista_fk`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `columnista`
+--
+ALTER TABLE `columnista`
+  MODIFY `id_columnista` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `podcast`
+--
+ALTER TABLE `podcast`
+  MODIFY `id_podcast` int(10) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
