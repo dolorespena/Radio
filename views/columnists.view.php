4fe24a76a1857c98f603a712af48f08aab42d0b1
Mauricio Oruezabal <moruezabal@gmail.com>
2020-05-06 11:04:32 -0300

    Ejemplo vista Columnistas

--- views/columnists.view.php
+++ views/columnists.view.php
@@ -4,9 +4,17 @@ Class ColumnistsView{
 
     public function showColumnists($columnists){
 
-        foreach ($columnists as $columnist);
-        echo($columnist->nombre);
+        echo ("<table><tr>");
+
+        foreach ($columnists as $columnist){
+            echo("<td>" . $columnist->nombre . "</td><td>" . $columnist->profesion . "</td><td>" . $columnist->descripcion . "</td><td><img src=" . BASE_URL . $columnist->url_imagen ."></td></tr>");
+        }
+
+        echo ("</table>");
+
+       
 
     }
 
+   
 }
