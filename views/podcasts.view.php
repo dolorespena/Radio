6dd1ec2e0c120500cf8adeb4116b64cba95c0c04
Mauricio Oruezabal <moruezabal@gmail.com>
2020-05-11 10:23:05 -0300

    header.tpl y /cache en gitignore

--- views/podcasts.view.php
+++ views/podcasts.view.php
@@ -4,39 +4,14 @@ Class PodcastsView{
 
     public function showPodcasts($podcasts){
         
-       $html = $this->showheader();
-       echo($html);
+       $smarty = new Smarty();
 
-        $columnist = $podcasts[0]->columnista; //Saco del arreglo el nombre del columnista 
-        echo ("<h1> Todas los podcasts de " . $columnist ."</h1>");
+       $smarty->assign('base_url', BASE_URL);
 
-        foreach ($podcasts as $podcast){ //Este ciclo se va a realizar por cada objeto del arreglo.
-            echo ("<h2>" . $podcast->nombre . "</h2>"); // Título del Podcast
-            echo("<p>" . $podcast->fecha . " - " . $podcast->duracion . " min</p>"); //Fecha y duración
-            echo("<p>" . $podcast->descripcion . "</p>"); // Descripcion del podcast
-            echo("<audio controls autoplay><source src='" . BASE_URL . $podcast->url_audio . "' ");
-            echo ("type='audio/ogg' codecs='vorbis'>Your browser does not support the element.</audio>"); 
-        
-        }
+       $smarty->assign('podcasts', $podcasts);
+       $smarty->assign('columnista',$podcasts[0]->columnista);
 
-        echo ("</body></html>");
-    }
-
-    private function showheader(){
-
-        echo (
-        '<!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <meta http-equiv="Content-Type" content="text/html">
-                <title>Columnistas</title>
-            </head>
-            <body>');
-
-
-
+       $smarty->display('podcasts.tpl');
     }
 
 }
