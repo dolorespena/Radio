a6667f4446ce9c778e1e146c975c92cb4d118f4f
Mauricio Oruezabal <moruezabal@gmail.com>
2020-05-22 15:33:22 -0300

    inserción de tablas en /admin

--- views/admin.view.php
+++ views/admin.view.php
@@ -3,8 +3,14 @@
 class AdminView{
 
     public function showAdmin($columnists, $podcasts){
-        var_dump($columnists);
-        var_dump($podcasts);
+        $smarty = new Smarty();
+
+        $smarty->assign('title', 'Columnistas');
+        $smarty->assign('base_url', BASE_URL);
+        $smarty->assign('columnists', $columnists);  
+        $smarty->assign('podcasts', $podcasts); 
+            
+        $smarty->display('admin.tpl');
     }
     
 }
