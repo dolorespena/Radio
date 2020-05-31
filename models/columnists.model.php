20b09aae6bcb8d4e3d4b1617e82489ec37b3c692
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-05-31 15:55:08 -0300

    modificacion en columnist, construct con inicio de conexion

--- models/columnists.model.php
+++ models/columnists.model.php
@@ -2,12 +2,17 @@
 
 Class ColumnistsModel{
 
+    private $db;
+
+    public function __construct(){
+        // 1. abro la conexión con MySQL 
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
+    }
+
     public function getAll(){
-         // 1. abro la conexión con MySQL 
-         $db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
-        
+
          // 2. enviamos la consulta (3 pasos)
-         $sentencia = $db->prepare("SELECT * FROM columnista"); // prepara la consulta
+         $sentencia = $this->db->prepare("SELECT * FROM columnista"); // prepara la consulta
          $sentencia->execute(); // ejecuta
          $columnists = $sentencia->fetchAll(PDO::FETCH_OBJ); // obtiene la respuesta
          
@@ -15,49 +20,35 @@ Class ColumnistsModel{
     }
 
     public function getColumnist($idColumnist){
-
-        $db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
-        
+ 
          // 2. enviamos la consulta (3 pasos)
-         $sentencia = $db->prepare("SELECT * FROM columnista  WHERE id_columnista = ?"); // prepara la consulta
+         $sentencia = $this->db->prepare("SELECT * FROM columnista  WHERE id_columnista = ?"); // prepara la consulta
          $sentencia->execute([$idColumnist]); // ejecuta
          $columnist = $sentencia->fetch(PDO::FETCH_OBJ); // obtiene la respuesta
          
-         return $columnist;   
-
+         return $columnist;  
     }
 
     public function insertColumnist($nombre, $profesion, $descripcion, $imagen){
-        // 1. abro la conexión con MySQL 
-        $db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
 
         // 2. enviamos la consulta
-        $sentencia = $db->prepare("INSERT INTO columnista (nombre, profesion, descripcion, url_imagen) VALUES(?, ?, ?, ?)"); // prepara la consulta
+        $sentencia = $this->db->prepare("INSERT INTO columnista (nombre, profesion, descripcion, url_imagen) VALUES(?, ?, ?, ?)"); // prepara la consulta
         $sentencia->execute([$nombre, $profesion, $descripcion, $imagen]); // ejecuta
-
     }
 
     public function updateColumnist($idColumnist, $nombre, $profesion, $descripcion, $imagen){
 
-        // 1. abro la conexión con MySQL 
-        $db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
-
         // 2. enviamos la consulta
-        $sentencia = $db->prepare("UPDATE columnista SET nombre = ?, profesion = ?, descripcion = ? , url_imagen = ? WHERE columnista.id_columnista = ?"); // prepara la consulta
+        $sentencia = $this->db->prepare("UPDATE columnista SET nombre = ?, profesion = ?, descripcion = ? , url_imagen = ? WHERE columnista.id_columnista = ?"); // prepara la consulta
         $success = $sentencia->execute([$nombre, $profesion, $descripcion, $imagen, $idColumnist]); // ejecuta
         return $success;
-
     }
 
     public function deleteColumnist($idColumnist){
 
-        // 1. abro la conexión con MySQL 
-        $db = new PDO('mysql:host=localhost;'.'dbname=db_radio;charset=utf8', 'root', '');
-
         // 2. enviamos la consulta
-        $sentencia = $db->prepare("DELETE FROM columnista WHERE id_columnista = ?"); // prepara la consulta
+        $sentencia = $this->db->prepare("DELETE FROM columnista WHERE id_columnista = ?"); // prepara la consulta
         $success = $sentencia->execute([$idColumnist]); // ejecuta
         return $success;
     }
-
 }
