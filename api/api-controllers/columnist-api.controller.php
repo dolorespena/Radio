a090381c270119ff6a41cd6f4df0ed9fff091e4b
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-06-26 20:33:00 -0300

    funcion deleteColumnist

--- api/api-controllers/columnist-api.controller.php
+++ api/api-controllers/columnist-api.controller.php
@@ -14,11 +14,31 @@ class ColumnistApiController{
     }
 
     public function getColumnists(){
-        $columnists = $this->model->getAll();
-        $this->view->responce($columnists);
+        $columnists = $this->model->getAll(); //obtengo los columnistas
+        $this->view->response($columnists, 200); //lo paso a la vista
     }
 
     public function getColumnist($params){
-        var_dump($params);
+        //obtengo el id de los params
+        $idColumnist = $params[':ID'];
+
+        $columnist = $this->model->getColumnist($idColumnist);// traigo el columnista con ese ID si existe
+        if ($columnist)
+            $this->view->response($columnist, 200); //lo mando a la vista
+       else
+            $this->view->response("No existe ese Columnista", 404);
+    }
+
+    public function deleteColumnist($params){
+        $idColumnist = $params[':ID'];
+        $columnist = $this->model->getColumnist($idColumnist);// traigo el columnista con ese ID si existe
+        
+        if(!empty($columnist))
+            $this->view->response("No existe el Columnista con id {$idColumnist}", 404);
+            die();
+        
+        // Si existe lo elimina
+        $this->model->deleteColumnist($idColumnist);//Elimino el columnista en el modelo
+        $this->view->response("El columnista con id {$idColumnist} se elimino correctamente", 200);// La vista muestra q fue borrado
     }
 }
