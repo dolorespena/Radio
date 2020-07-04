baa8246c873edbedea4becd342b53c573e4d1f5b
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-07-04 20:02:56 -0300

    comment.vue

---
+++ templates/vue.js/comments.vue
@@ -0,0 +1,21 @@
+{literal}
+
+    <section id="templateVueComments">
+    
+       <li v-for="comment in comments">
+        <h3>{{comment.username}}</h3>
+       <span> {{comment.detalle}}  </span>
+       <span> {{comment.fecha}}</span>
+
+
+        <!--   
+        <span v-if="task.finalizada == 0 && auth">
+                <a data-id="{{task.id}}" class="btn-eliminar" href="#">eliminar</a>
+                <a data-id="{{task.id}}" class="btn-completar" href="#">completar</a>
+        </span> 
+        -->
+       </li> 
+
+
+    </section>
+{/literal}
