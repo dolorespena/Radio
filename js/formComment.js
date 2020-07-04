de84d3ca31c0bf6232b3482fb41980ae1c47a6b9
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-07-04 20:03:35 -0300

    funcion getComment en js

--- js/formComment.js
+++ js/formComment.js
@@ -3,6 +3,31 @@
 let form = document.querySelector('#formComment');
 form.addEventListener('submit', addComment);
 
+getComments();
+
+// define la app Vue
+let app = new Vue({
+    el: "#templateVueComments",
+    data: {
+        subtitle: "Comentario utilizando vue.js",
+        comments: [], 
+        auth: true
+    }
+});
+
+function getComments() {
+
+    let id_podcast = document.querySelector("#id_podcast").value;
+    fetch("api/comentarios/" + id_podcast)
+    .then(response => response.json())
+    .then(comments => {
+        app.comments = comments; //asignacion de variable comments para el templates
+    })
+    .catch(error => console.log(error));
+}
+
+
+
 function addComment(){
     event.preventDefault();
     let f = new Date;
@@ -19,18 +44,14 @@ function addComment(){
         "id_podcast_fk" : podcast,
         "id_usuario_fk" : user
     };
-
-    console.log(comment);
     
-    fetch('http://localhost:80/radio/api/comentario/', {
+    fetch('api/comentario/', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},       
         body: JSON.stringify(comment) 
-     })
-     .then(response => {
+    }).then(response => {
          console.log(response);
-     })
-     .catch(error => console.log(error));
+    }).catch(error => console.log(error));
 }
 
 
