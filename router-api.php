922531a4ed0ff0d7638583283b141c35887ba925
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-07-03 22:50:31 -0300

    funciones de comentario en router-api

--- router-api.php
+++ router-api.php
@@ -1,18 +1,18 @@
 <?php
 
 require_once 'libs/router/Router.php';
-require_once 'api/api-controllers/columnist-api.controller.php';
+require_once 'api/api-controllers/comment-api.controller.php';
 
 // creo el ruteador
 $router = new Router();
 
 //creo la tabla de ruteo
 
-//--------- Columnist ----------
-$router->addRoute('columnistas', 'GET', 'ColumnistApiController', 'getColumnists');
-$router->addRoute('columnista/:ID', 'GET', 'ColumnistApiController','getColumnist');
-$router->addRoute('columnista/:ID', 'DELETE', 'ColumnistApiController', 'deleteColumnist');
-
+//--------- Comment ----------
+$router->addRoute('comentarios', 'GET', 'CommentApiController', 'getComments');
+$router->addRoute('comentario/:ID', 'GET', 'CommentApiController', 'getComment');
+$router->addRoute('comentario/:ID', 'DELETE', 'CommentApiController', 'deleteComment');
+$router->addRoute('comentario', 'POST', 'CommentApiController', 'insertComment');
 
 //ruteo
 $router->route($_REQUEST['resource'], $_SERVER['REQUEST_METHOD']);
